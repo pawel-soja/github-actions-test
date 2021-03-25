1eed8d2f768ada69a95c91a10ba2740741cfc379
Pawel Soja <kernel32.pl@gmail.com>
2021-03-26 00:35:01 +0100

    WIP

--- scripts/requisites-install.sh
+++ scripts/requisites-install.sh
@@ -4,32 +4,45 @@ set -e
 
 . /etc/os-release
 
+# libtiff-devel ?
+
 case $ID in
     debian|ubuntu)
         export DEBIAN_FRONTEND=noninteractive
         $(command -v sudo) apt-get update
         $(command -v sudo) apt-get -y install \
-            libnova-dev libcfitsio-dev libusb-1.0-0-dev zlib1g-dev \
-            libgsl-dev build-essential cmake git libjpeg-dev \
-            libcurl4-gnutls-dev libtiff-dev libfftw3-dev
+            git \
+            cmake build-essential zlib1g-dev \
+            libcfitsio-dev libnova-dev libusb-1.0-0-dev libcurl4-gnutls-dev \
+            libgsl-dev libjpeg-dev libfftw3-dev
         ;;
     fedora)
         $(command -v sudo) dnf -y install \
-            libnova-devel cfitsio-devel libusb-devel zlib-devel \
-            gsl-devel cmake git libjpeg-devel \
-            libcurl-devel libtiff-devel fftw-devel \
-            gcc-c++
+            git \
+            cmake gcc-c++ zlib-devel \
+            cfitsio-devel libnova-devel libusb-devel libcurl-devel \
+            gsl-devel libjpeg-devel fftw-devel
         ;;
     centos)
+        # CentOS 8 dont have libnova-devel package
         $(command -v sudo) yum -y install epel-release
         $(command -v sudo) yum -y upgrade
         $(command -v sudo) yum -y install \
-            libnova-devel cfitsio-devel libusbx-devel libusb-devel zlib-devel \
-            gsl-devel cmake git libjpeg-devel \
-            libcurl-devel libtiff-devel fftw-devel
+            git \
+            cmake gcc-c++ zlib-devel \
+            cfitsio-devel libnova-devel libusb-devel libcurl-devel \
+            gsl-devel libjpeg-devel fftw-devel
         ;;
 
-        # CentOS 8 dont have libnova-devel package
+    opensuse-tumbleweed)
+        # broken git/openssh package
+        # zypper refresh && zypper  --non-interactive update
+        $(command -v sudo) zypper --non-interactive install -y \
+            openssh git \
+            cmake gcc-c++ zlib-devel \
+            cfitsio-devel libnova-devel libusb-devel libcurl-devel \
+            gsl-devel libjpeg-devel fftw-devel libtheora-devel
+        ;;
     *)
         echo "Unknown Linux Distribution"
         cat /etc/os-release
