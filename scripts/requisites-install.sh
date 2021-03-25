9e3fd781a0eca3c1ab26c949098b14b91a0a6c0c
Pawel Soja <kernel32.pl@gmail.com>
2021-03-25 18:11:03 +0100

    WIP

--- scripts/requisites-install.sh
+++ scripts/requisites-install.sh
@@ -6,6 +6,7 @@ set -e
 
 case $ID in
     debian|ubuntu)
+        ls -l /var/cache/apt
         export DEBIAN_FRONTEND=noninteractive
         $(command -v sudo) apt-get update
         $(command -v sudo) apt-get -y install \
@@ -14,6 +15,7 @@ case $ID in
             libcurl4-gnutls-dev libtiff-dev libfftw3-dev
         ;;
     fedora)
+        ls -l /var/cache/dnf
         $(command -v sudo) dnf -y install \
             libnova-devel cfitsio-devel libusb-devel zlib-devel \
             gsl-devel cmake git libjpeg-devel \
@@ -21,6 +23,7 @@ case $ID in
             gcc-c++
         ;;
     centos)
+        ls -l /var/cache/yum
         $(command -v sudo) yum -y install epel-release
         $(command -v sudo) yum -y upgrade
         $(command -v sudo) yum -y install \
