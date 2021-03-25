68e29a8e9fec1ac4c2f4ae841f08a71cb0a25ec1
Pawel Soja <kernel32.pl@gmail.com>
2021-03-25 17:46:20 +0100

    WIP

--- scripts/requisites-install.sh
+++ scripts/requisites-install.sh
@@ -25,9 +25,9 @@ case $ID in
         $(command -v sudo) yum -y install epel-release
         $(command -v sudo) yum -y upgrade
         $(command -v sudo) yum -y install \
-            libnova-devel libcfitsio-devel libusb-1.0-0-devel zlib1g-devel \
-            libgsl-devel build-essential cmake git libjpeg-devel \
-            libcurl4-gnutls-devel libtiff-devel libfftw3-devel
+            libnova-devel cfitsio-devel libusbx-devel libusb-devel zlib-devel \
+            gsl-devel cmake git libjpeg-devel \
+            libcurl-devel libtiff-devel fftw-devel
         ;;
 
         # CentOS 8 dont have libnova-devel package
