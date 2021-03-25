bc03d31b9f92a93285af2ee945cbc192d752f3c5
Pawel Soja <kernel32.pl@gmail.com>
2021-03-25 17:11:28 +0100

    WIP

--- scripts/requisites-install.sh
+++ scripts/requisites-install.sh
@@ -6,9 +6,9 @@ set -e
 
 case $ID in
     debian|ubuntu)
-        $(command -v sudo) apt-get -qq update
-        $(command -v sudo) apt-get -qqy dist-upgrade
-        $(command -v sudo) apt-get -qqy install \
+        apt-get -qq update
+        apt-get -qqy dist-upgrade
+        apt-get -qqy install \
             libnova-dev libcfitsio-dev libusb-1.0-0-dev zlib1g-dev \
             libgsl-dev build-essential cmake git libjpeg-dev \
             libcurl4-gnutls-dev libtiff-dev libfftw3-dev
@@ -17,11 +17,12 @@ case $ID in
         $(command -v sudo) dnf -y install \
             libnova-devel cfitsio-devel libusb-devel zlib-devel \
             gsl-devel build-essential cmake git libjpeg-devel \
-            libcurl-devel libtiff-devel fftw-devel
+            libcurl-devel libtiff-devel fftw-devel \
+            gcc-c++
         ;;
     centos)
         echo "Hello Centos"
-        yum -y install install \
+        $(command -v sudo) yum -y install \
             libnova-devel libcfitsio-devel libusb-1.0-0-devel zlib1g-devel \
             libgsl-devel build-essential cmake git libjpeg-devel \
             libcurl4-gnutls-devel libtiff-devel libfftw3-devel
