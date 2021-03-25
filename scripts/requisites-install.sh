935f8ccd660a0704cd707f40bf8ba24904df4ea6
Pawel Soja <kernel32.pl@gmail.com>
2021-03-25 17:14:02 +0100

    WIP

--- scripts/requisites-install.sh
+++ scripts/requisites-install.sh
@@ -6,6 +6,7 @@ set -e
 
 case $ID in
     debian|ubuntu)
+        # command ubuntu unknown fail
         apt-get -qq update
         apt-get -qqy dist-upgrade
         apt-get -qqy install \
@@ -16,12 +17,12 @@ case $ID in
     fedora)
         $(command -v sudo) dnf -y install \
             libnova-devel cfitsio-devel libusb-devel zlib-devel \
-            gsl-devel build-essential cmake git libjpeg-devel \
+            gsl-devel cmake git libjpeg-devel \
             libcurl-devel libtiff-devel fftw-devel \
             gcc-c++
         ;;
     centos)
-        echo "Hello Centos"
+        $(command -v sudo) yum check-update
         $(command -v sudo) yum -y install \
             libnova-devel libcfitsio-devel libusb-1.0-0-devel zlib1g-devel \
             libgsl-devel build-essential cmake git libjpeg-devel \
