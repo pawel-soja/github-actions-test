d74c93fa693af0325fe8c8a6a06ca52473557173
Pawel Soja <kernel32.pl@gmail.com>
2021-03-29 00:24:51 +0200

    WIP

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,7 +6,17 @@ set -e
 # whoami
 # nproc
 
-brew install git cfitsio
+# apt-get update && apt-get -y upgrade && apt-get install -y \
+#    git \
+#    cmake build-essential zlib1g-dev \
+#    libcfitsio-dev libnova-dev libusb-1.0-0-dev libcurl4-gnutls-dev \
+#    libgsl-dev libjpeg-dev libfftw3-dev
+#
+
+brew install \
+    git \
+    cfitsio libnova libusb curl \
+    libgsl jpeg fftw
 
 pushd /tmp
 git clone --depth=1 --branch=fix-macos https://github.com/indilib/indi.git
