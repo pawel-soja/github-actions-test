46732dbf9fa4c1131ad2a9f024f3a422537a6933
Pawel Soja <kernel32.pl@gmail.com>
2021-03-29 00:07:47 +0200

    WIP

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,6 +4,15 @@
 # alias nproc="sysctl -n hw.logicalcpu"
 # whoami
 # nproc
+command -v realpath >/dev/null 2>&1 || realpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+echo $(realpath $0)
+SRCS=$(dirname $(realpath $0))/..
+echo $SRCS
+
+exit 0
 
 brew install git
 
