ad2f66e15ae1928c5536521b00bfd6d2a443927c
Pawel Soja <kernel32.pl@gmail.com>
2021-03-29 00:12:01 +0200

    WIP

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,20 +4,11 @@
 # alias nproc="sysctl -n hw.logicalcpu"
 # whoami
 # nproc
-command -v realpath >/dev/null 2>&1 || realpath() {
-    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
-}
-
-echo $(realpath $0)
-SRCS=$(dirname $(realpath $0))/..
-echo $SRCS
-
-exit 0
 
 brew install git
 
 pushd /tmp
-git clone --depth=1 https://github.com/indilib/indi.git
+git clone --depth=1 --branch=fix-macos https://github.com/indilib/indi.git
 ./indi/scripts/indi-core-build.sh
 pwd
 popd
