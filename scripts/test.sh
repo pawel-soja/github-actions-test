af450ba09594222404374cc7c92e8245d9fdd556
Pawel Soja <kernel32.pl@gmail.com>
2021-03-29 16:12:13 +0200

    WIP

--- scripts/test.sh
+++ scripts/test.sh
@@ -18,11 +18,12 @@ brew install \
     cfitsio libnova libusb curl \
     gsl jpeg fftw
 
-tar --help
-bsdtar --help
-exit 0
 pushd /tmp
 git clone --depth=1 --branch=fix-macos https://github.com/indilib/indi.git
+./indi/scripts/googletest-build.sh
+./indi/scripts/googletest-install.sh
 ./indi/scripts/indi-core-build.sh
+./indi/scripts/indi-core-install.sh
+
 pwd
 popd
