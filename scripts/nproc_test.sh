066e54d2ae3465a96a0a3b50bf6f01c3292bf681
Pawel Soja <kernel32.pl@gmail.com>
2021-03-30 12:26:18 +0200

    WIP

--- scripts/nproc_test.sh
+++ scripts/nproc_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-command -v np2roc >/dev/null 2>&1 || function nproc {
+command -v nproc >/dev/null 2>&1 || function nproc {
     command -v sysctl >/dev/null 2>&1 &&  \
         sysctl -n hw.logicalcpu ||
         echo "3"
