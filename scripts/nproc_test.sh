fb81a466a2c5fb6410b3be54caee706446cb51fd
Pawel Soja <kernel32.pl@gmail.com>
2021-03-30 12:40:09 +0200

    WIP

--- scripts/nproc_test.sh
+++ scripts/nproc_test.sh
@@ -3,7 +3,7 @@
 command -v nproc >/dev/null 2>&1 || function nproc {
     command -v sysctl >/dev/null 2>&1 &&  \
         sysctl -n hw.logicalcpu ||
-        echo "3"
+        echo "13"
 }
 
 echo $(($(nproc)+1))
